f89dc1925a5fccb0b6ea46aa2c2d546f965813c5
Dawid Olczak <dawid.a.olczak@gmail.com>
2023-08-02 20:43:25 +0200

    fix

--- src/store/store.service.ts
+++ src/store/store.service.ts
@@ -69,11 +69,12 @@ export class StoreService {
     try {
       const store_url = process.env.WOOCOMMERCE_STORE_URL
       const store = await StoreEntity.findOneBy({url: store_url})
+      const oldToken = store.furgonetka_access_token
       const furgonetka_access_token = await getToken();
       store.furgonetka_access_token = furgonetka_access_token
       await store.save()
       return createResponse(true, 'Pomyślnie zaaktualizowano token',200, {
-        old_token: store.furgonetka_access_token,
+        old_token: oldToken,
         new_token: furgonetka_access_token,
       })
     } catch(e) {
