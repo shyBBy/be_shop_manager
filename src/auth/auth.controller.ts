44b19f5508cf18c689d9aa94631ce14cc83ddcba
Dawid Olczak <dawid.a.olczak@gmail.com>
2023-11-05 18:27:40 +0100

    added wpLogin endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Post, Res, Req, UseGuards } from '@nestjs/common';
+import {Controller, Get, Post, Res, Req, UseGuards, Body} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Response } from 'express';
 import { UserObj } from '../decorators/user-object.decorator';
 import { UserEntity } from '../user/entities/user.entity';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import {WpLoginDto} from "./dto/wp-login.dto";
 
 @Controller('auth')
 export class AuthController {
@@ -14,6 +15,24 @@ export class AuthController {
   @Post('login')
   async login(@UserObj() user: UserEntity, @Res() res: Response) {
     return this.authService.login(user, res);
+  }
+
+  @Post('wp-login')
+  @UseGuards(JwtAuthGuard)
+  async wpLogin(
+      @UserObj() user: UserEntity,
+      @Body() wpLoginDto: WpLoginDto,
+      @Res() res: Response,
+  ) {
+    return this.authService.wpLogin(wpLoginDto, user, res);
+  }
+
+  @Get('wp-login/token')
+  @UseGuards(JwtAuthGuard)
+  async checkWpToken(
+      @UserObj() user: UserEntity
+  ) {
+    return this.authService.checkWpToken(user);
   }
 
   @UseGuards(JwtAuthGuard)
