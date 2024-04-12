2f4fddccc4b2694510423065703b49af8253c1c6
Dawid Olczak <dawid.a.olczak@gmail.com>
2024-04-12 18:22:07 +0200

    foix

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -3,12 +3,18 @@ import { ProductService } from './product.service';
 import {JwtAuthGuard} from "../guards/jwt-auth.guard";
 import {UserObj} from "../decorators/user-object.decorator";
 import {UserEntity} from "../user/entities/user.entity";
-import {GetListOfAllOrdersResponse} from "../../types/order/order";
 import {GetListOfAllProductsResponse} from "../../types/product/product";
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Get('/list')
+  @UseGuards(JwtAuthGuard)
+  getAll(@UserObj() user: UserEntity): Promise<GetListOfAllProductsResponse> {
+    console.log('w CONTROLLER')
+    return this.productService.getAllProducts(user.id);
+  }
 
   @Get('/:ean')
   @UseGuards(JwtAuthGuard)
@@ -16,9 +22,4 @@ export class ProductController {
     return this.productService.getOneByEan(ean, user.id);
   }
 
-  @Get('/list')
-  @UseGuards(JwtAuthGuard)
-  getAll(@UserObj() user: UserEntity): Promise<GetListOfAllProductsResponse> {
-    return this.productService.getAllProducts(user.id);
-  }
 }
